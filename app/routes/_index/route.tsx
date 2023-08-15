9e904d44e0a54bd0f8f2edf588f08d8172504131
Mészáros Barnabás <barnabas.meszaros.development@gmail.com>
2023-08-15 18:33:01 +0200

    FAQ (without content)

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,5 +1,9 @@
 import { useTranslation } from 'react-i18next';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
 import ElectricCarIcon from '@mui/icons-material/ElectricCar';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Grid from '@mui/material/Unstable_Grid2';
 import Grow from '@mui/material/Grow';
 import List from '@mui/material/List';
@@ -43,6 +47,33 @@ export default function Index() {
                     <Typography align={'center'} variant={'h6'}>
                         {t('home_description')}
                     </Typography>
+                    <Typography align={'center'} variant={'h4'}>
+                        {t('home_title_faq')}
+                    </Typography>
+                    <Accordion>
+                        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                            <Typography>{t('home_faq_1_title')}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>{t('home_faq_1_description')}</AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                            <Typography>{t('home_faq_2_title')}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>{t('home_faq_2_description')}</AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                            <Typography>{t('home_faq_3_title')}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>{t('home_faq_3_description')}</AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                            <Typography>{t('home_faq_4_title')}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>{t('home_faq_4_description')}</AccordionDetails>
+                    </Accordion>
                 </Grid>
             </Grid>
         </Grow>
