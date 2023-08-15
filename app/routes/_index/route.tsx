437791c65cc189b94e9697aaae23fab94cbb0b28
Mészáros Barnabás <barnabas.meszaros.development@gmail.com>
2023-08-15 11:00:11 +0200

    Update route.tsx

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,6 +1,13 @@
 import { useTranslation } from 'react-i18next';
+import ElectricCarIcon from '@mui/icons-material/ElectricCar';
 import Grid from '@mui/material/Unstable_Grid2';
 import Grow from '@mui/material/Grow';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MapsHomeWorkIcon from '@mui/icons-material/MapsHomeWork';
+import SolarPowerIcon from '@mui/icons-material/SolarPower';
 import Typography from '@mui/material/Typography';
 
 export default function Index() {
@@ -8,18 +15,35 @@ export default function Index() {
 
     return (
         <Grow in={true}>
-            <Grid p={2} xs={12}>
-                <Typography variant={'h4'}>{t('home_title')}</Typography>
-                <Typography color={'text.secondary'} variant={'h6'}>
-                    {t('service_1_description')}
-                </Typography>
-                <Typography color={'text.secondary'} variant={'h6'}>
-                    {t('service_2_description')}
-                </Typography>
-                <Typography color={'text.secondary'} variant={'h6'}>
-                    {t('service_10_description')}
-                </Typography>
-                <Typography variant={'h6'}>{t('home_description')}</Typography>
+            <Grid container justifyContent={'center'} p={2} xs={12}>
+                <Grid md={6} sm={10} xs={12}>
+                    <Typography align={'center'} variant={'h4'}>
+                        {t('home_title')}
+                    </Typography>
+                    <List>
+                        <ListItem>
+                            <ListItemIcon>
+                                <SolarPowerIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={t('service_1_description')} />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemIcon>
+                                <MapsHomeWorkIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={t('service_2_description')} />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemIcon>
+                                <ElectricCarIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={t('service_10_description')} />
+                        </ListItem>
+                    </List>
+                    <Typography align={'center'} variant={'h6'}>
+                        {t('home_description')}
+                    </Typography>
+                </Grid>
             </Grid>
         </Grow>
     );
