652e331109ed5d30000165c969c47fb3b0c839df
Mészáros Barnabás <barnabas.meszaros.development@gmail.com>
2023-10-01 09:24:12 +0200

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -20,7 +20,7 @@ interface DocumentProps {
 }
 
 export const headers: HeadersFunction = () => ({
-    'Content-Security-Policy': "default-src 'self'; connect-src https://vitals.vercel-insights.com",
+    'Content-Security-Policy': "default-src 'self'; img-src *; connect-src vitals.vercel-insights.com",
 });
 
 export const loader: LoaderFunction = async ({ request }: LoaderArgs) => ({ locale: await i18next.getLocale(request) });
