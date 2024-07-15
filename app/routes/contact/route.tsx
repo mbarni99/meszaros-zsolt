6861451a45ce701c3c682d2652e2ce031ab3dc84
Mészáros Barnabás <barnabas.meszaros.development@gmail.com>
2024-07-15 19:43:02 +0200

    cleaning up

--- app/routes/contact/route.tsx
+++ app/routes/contact/route.tsx
@@ -1,65 +1,16 @@
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment, useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import { useTranslation } from 'react-i18next';
-import { Form, useActionData, useNavigation } from '@remix-run/react';
 import Alert from '@mui/material/Alert';
 import Chip from '@mui/material/Chip';
-import ClassIcon from '@mui/icons-material/Class';
 import EmailIcon from '@mui/icons-material/Email';
 import Grid from '@mui/material/Unstable_Grid2';
 import Grow from '@mui/material/Grow';
 import HomeIcon from '@mui/icons-material/Home';
-import InputAdornment from '@mui/material/InputAdornment';
-import LoadingButton from '@mui/lab/LoadingButton';
 import PhoneIcon from '@mui/icons-material/Phone';
-import SendIcon from '@mui/icons-material/Send';
-import sgMail from '@sendgrid/mail';
 import Snackbar from '@mui/material/Snackbar';
-import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import { z } from 'zod';
-import type { ActionArgs, ActionFunction, V2_MetaFunction } from '@remix-run/node';
-
-const lengths = { message: 1000, sender: 50, subject: 100 };
-
-const ContactSchema = z.object({
-    message: z.string({ required_error: 'contactschema_message_required' }).max(lengths.message, 'contactschema_message_max_length'),
-    sender: z
-        .string({ required_error: 'contactschema_sender_required' })
-        .max(lengths.sender, 'contactschema_sender_max_length')
-        .email('contactschema_sender_email'),
-    subject: z.string({ required_error: 'contactschema_subject_required' }).max(lengths.subject, 'contactschema_subject_max_length'),
-});
-
-type ContactSchema = z.infer<typeof ContactSchema>;
-
-export const action: ActionFunction = async ({ request }: ActionArgs) => {
-    const formData = await request.formData();
-
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
-
-    const contactInput = {
-        message: formData.get('message') as string,
-        sender: formData.get('sender') as string,
-        subject: formData.get('subject') as string,
-    };
-
-    const validatedContactInput: ContactSchema = ContactSchema.parse(contactInput);
-
-    const message = {
-        to: 'zsoltm340@gmail.com',
-        from: 'zsoltm340@gmail.com',
-        subject: validatedContactInput.subject,
-        text: validatedContactInput.message,
-    };
-
-    try {
-        await sgMail.send(message);
-    } catch (error: any) {
-        error?.response?.body.errors?.map((error: any) => console.error(error.message));
-        return error;
-    }
-};
+import type { V2_MetaFunction } from '@remix-run/node';
 
 export const meta: V2_MetaFunction = () => [
     { name: 'description', content: 'Keressen minket bizalommal!' },
@@ -71,15 +22,7 @@ export default function Contact() {
 
     const isMobile = useMediaQuery({ query: '(max-width: 640px)' });
 
-    const actionData = useActionData();
-    const { state } = useNavigation();
-
-    const [message, setMessage] = useState<string>('');
     const [open, setOpen] = useState<boolean>(false);
-    const [sender, setSender] = useState<string>('');
-    const [subject, setSubject] = useState<string>('');
-
-    useEffect(() => setOpen(actionData?.[0]?.statusCode === 202), [JSON.stringify(actionData)]);
 
     return (
         <Fragment>
@@ -108,7 +51,7 @@ export default function Contact() {
                         </section>
                     </Grid>
                 </Grow>
-                <Grow in>
+                {/* <Grow in>
                     <Grid md={5} p={1} sm={6} xl={4} xs={12}>
                         <section id={'section-contact-form'}>
                             <Grid pb={2} xs={12}>
@@ -170,7 +113,7 @@ export default function Contact() {
                             </Form>
                         </section>
                     </Grid>
-                </Grow>
+                </Grow> */}
             </Grid>
         </Fragment>
     );
